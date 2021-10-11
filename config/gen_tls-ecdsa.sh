501af62786fe334a94baa583a21f34732179d74f
Damrongsak Reetanon <damrongs@gmail.com>
2021-10-11 17:49:57 +0700

    Update gen_tls-ecdsa.sh

--- config/gen_tls-ecdsa.sh
+++ config/gen_tls-ecdsa.sh
@@ -8,7 +8,7 @@ rm -f server*
 
 openssl ecparam -out server1.key -name prime256v1 -genkey
 openssl req -new -key server1.key -out server1.csr -sha256 -subj "/CN=server1"
-echo subjectAltName = DNS:$(hostname),IP:$(ip a show dev enp1s0 |grep "inet "|awk '{print $2}'|cut -d "/" -f 1),IP:127.0.0.1 >> server1.cnf
+echo subjectAltName = DNS:$(hostname),IP:$(ip a show dev eth0 |grep "inet "|awk '{print $2}'|cut -d "/" -f 1),IP:127.0.0.1 >> server1.cnf
 echo extendedKeyUsage = serverAuth >> server1.cnf
 openssl x509 -req -in server1.csr -CAkey ca.key -CA ca.crt -out server1.crt -CAcreateserial -extfile server1.cnf
 
